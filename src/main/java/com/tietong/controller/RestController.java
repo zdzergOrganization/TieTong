e63d9a0082e898139c010fcdd3fe9e977baad466
夜书 <zhouding.zd@alibaba-inc.com>
2015-09-24 16:47:38 +0800

    插入数据库成功

--- src/main/java/com/tietong/controller/RestController.java
+++ src/main/java/com/tietong/controller/RestController.java
@@ -1,6 +1,8 @@
 package com.tietong.controller;
 
-import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import com.tietong.dao.TieTongMapper;
 import com.tietong.pojo.Employee;
@@ -21,28 +24,34 @@ public class RestController {
 	@Autowired
 	private TieTongMapper tieTongMapper;
 	
+	//private Employee employee = new Employee();
+	
 	@RequestMapping(value="/employee/batchAddCommit" , method=RequestMethod.GET)
-	public @ResponseBody void batchAddCommit(String filePath){
-		
+	public @ResponseBody void batchAddCommit(HttpServletRequest request){
+		Map<String,?> map = RequestContextUtils.getInputFlashMap(request);   
+		//读取文件路径
+		String filePath = (String) map.get("filePath");
 		//读取批量导入的员工信息
 		ReadExcel readExcel = new ReadExcel();
 		Workbook wb  = readExcel.read(filePath);
         Sheet sheet = wb.getSheetAt(0);     //获得第一个表单  
-        
+
+    	Employee employee = new Employee();
         //去掉表头，从第一行取数据
-        for(int i=1;i<sheet.getLastRowNum();i++){
-        	Employee employee = null;
+        for(int i=1;i<=sheet.getLastRowNum();i++){
+        	//Employee employee = new Employee();
             Row row = sheet.getRow(i);
-            employee.setEmployee_name(row.getCell(0).toString());
-            employee.setType(row.getCell(1).toString());
-            employee.setRegion_pq(row.getCell(2).toString());
-            employee.setRegion_q(row.getCell(3).toString());
-            employee.setRegion_wg(row.getCell(4).toString());
-            employee.setEntry_date(row.getCell(5).toString());
-            employee.setQuit_date(row.getCell(6).toString());
+            String s = readExcel.getValue(row.getCell(0));
+            employee.setEmployee_name(s);
+            employee.setType(readExcel.getValue(row.getCell(1)));
+            employee.setRegion_pq(readExcel.getValue(row.getCell(2)));
+            employee.setRegion_q(readExcel.getValue(row.getCell(3)));
+            employee.setRegion_wg(readExcel.getValue(row.getCell(4)));
+            employee.setEntry_date(readExcel.getValue(row.getCell(5)));
+            employee.setQuit_date(readExcel.getValue(row.getCell(6)));
             
             //先不输入计算月份
-            //employee.setMonth_end_date(row.getCell(7).toString());
+            //employee.setMonth_end_date(readExcel.getValue(row.getCell(7)));
             
             //插入数据库
             tieTongMapper.insertEmployeeInfo(employee);
--- src/main/java/com/tietong/web/util/ReadExcel.java
+++ src/main/java/com/tietong/web/util/ReadExcel.java
@@ -2,16 +2,16 @@ package com.tietong.web.util;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ReadExcel {
-	 public static Workbook read(String filePath){  
+	 public Workbook read(String filePath){  
 	        boolean isE2007 = false;    //判断是否是excel2007格式  
 	        if(filePath.endsWith("xlsx"))  
 	            isE2007 = true;  
@@ -31,5 +31,35 @@ public class ReadExcel {
 	        }
 			return null;  
 	    }  
+	 
+ /*private String getValue(XSSFCell xssfRow) {
+     if (xssfRow.getCellType() == xssfRow.CELL_TYPE_BOOLEAN) {
+         return String.valueOf(xssfRow.getBooleanCellValue());
+     } else if (xssfRow.getCellType() == xssfRow.CELL_TYPE_NUMERIC) {
+         return String.valueOf(xssfRow.getNumericCellValue());
+     } else {
+         return String.valueOf(xssfRow.getStringCellValue());
+     }
+ }
+
+ private String getValue(HSSFCell hssfCell) {
+     if (hssfCell.getCellType() == hssfCell.CELL_TYPE_BOOLEAN) {
+         return String.valueOf(hssfCell.getBooleanCellValue());
+     } else if (hssfCell.getCellType() == hssfCell.CELL_TYPE_NUMERIC) {
+         return String.valueOf(hssfCell.getNumericCellValue());
+     } else {
+         return String.valueOf(hssfCell.getStringCellValue());
+     }
+ }*/
+ 
+ public String getValue(Cell cell) {
+     if (cell.getCellType() == cell.CELL_TYPE_BOOLEAN) {
+         return String.valueOf(cell.getBooleanCellValue());
+     } else if (cell.getCellType() == cell.CELL_TYPE_NUMERIC) {
+         return String.valueOf(cell.getNumericCellValue());
+     } else {
+         return cell.getStringCellValue();
+     }
+ }
 	 
 }
